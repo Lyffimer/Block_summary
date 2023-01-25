2d67ccf3cde6cce1ea5a5ddb9ce916250bdc5d3f
Igor_Deulin <lyffimer@bk.ru>
2023-01-25 23:50:50 +0300

    add solution

--- taskBS/Program.cs
+++ taskBS/Program.cs
@@ -2,9 +2,27 @@
 // string[] start = { "1234", "1567", "-2", "computer science"};
 // string[] start = { "Russia", "Denmark", "Kazan"};
 
-string[] end = (from elm in start 
-                where (elm.Length) <= 3 
-                select elm).ToArray();
+string[] NewStringArray(string[]array)
+{
+    int finLength = 0;
+    for (int oldIndex = 0; oldIndex < array.Length; oldIndex++)
+    {
+        if(array[oldIndex].Length<=3) finLength++;
+    }
+    string[] resaltArr = new string [finLength];
+    int newIndex = 0;
+    for (int oldIndex = 0; oldIndex < array.Length; oldIndex++)
+    {
+        if(array[oldIndex].Length<=3)
+        {
+            resaltArr[newIndex] = array[oldIndex];
+            newIndex++;
+        }
+    }
+    return resaltArr;
+}
+
+string [] end = NewStringArray(start);
 
 System.Console.WriteLine($"Старый массив из {start.Count()} элементов = [ {string.Join(", ", start)} ]");
 System.Console.WriteLine($"Новый массив из {end.Count()} элементов = [ {string.Join(", ", end)} ]");
