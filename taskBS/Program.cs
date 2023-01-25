eb9ccf9f1d0792130dc1040443f195fe4d34f8ce
Igor_Deulin <lyffimer@bk.ru>
2023-01-25 23:48:12 +0300

    add LINQ

---
+++ taskBS/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] start = { "hello", "2", "world", ":-)"};
+// string[] start = { "1234", "1567", "-2", "computer science"};
+// string[] start = { "Russia", "Denmark", "Kazan"};
+
+string[] end = (from elm in start 
+                where (elm.Length) <= 3 
+                select elm).ToArray();
+
+System.Console.WriteLine($"Старый массив из {start.Count()} элементов = [ {string.Join(", ", start)} ]");
+System.Console.WriteLine($"Новый массив из {end.Count()} элементов = [ {string.Join(", ", end)} ]");
